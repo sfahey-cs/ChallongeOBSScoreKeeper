871c2f9faf6f8d91db7023b85b4d0cc3ac40bbfd
Sarah Fahey <sfahey.cs@gmail.com>
2016-01-05 14:59:50 -0500

    MatchComboBox functioning

--- ChallongeOBSScoreKeeper/MainWindow.xaml.cs
+++ ChallongeOBSScoreKeeper/MainWindow.xaml.cs
@@ -1,7 +1,7 @@
 ﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Windows;
-using System.Data;
 
 namespace ChallongeOBSScoreKeeper
 {
@@ -29,6 +29,71 @@ namespace ChallongeOBSScoreKeeper
                 playerListView.Items.Add(data[i].Participant.name);
             }
         }
+
+        private void comboBox_DropDownOpened(object sender, System.EventArgs e)
+        {
+            comboBox.Items.Clear();
+
+            string endPointParticipants = @"https://api.challonge.com/v1/tournaments/1143489/participants.json";
+            var clientParticipants = new RestClient(endPointParticipants);
+            var jsonParticipants = clientParticipants.MakeRequest("?api_key=dRmpIHLRpgfiepsGWcixpRrettoVeYmSqPw2RJcg");
+            JsonSerializer serializerParticipants = new JsonSerializer();
+            var dataParticipants = JsonConvert.DeserializeObject<List<Participants>>(jsonParticipants);
+
+            string endPoint = @"https://api.challonge.com/v1/tournaments/1143489/matches.json";
+            var client = new RestClient(endPoint);
+            var json = client.MakeRequest("?api_key=dRmpIHLRpgfiepsGWcixpRrettoVeYmSqPw2RJcg");
+            JsonSerializer serializer = new JsonSerializer();
+            var dataMatches = JsonConvert.DeserializeObject<List<Matches>>(json);
+
+            var openMatches = new List<SingleMatch>();
+            string player1Name = "";
+            string player2Name = "";
+            string round = "Winner's";
+
+            int finalRound = dataMatches[dataMatches.Count-1].match.round;
+
+            for (int i=0;i<dataMatches.Count;i++)
+            {
+                if (dataMatches[i].match.state.Equals("open"))
+                {
+                    for(int j=0; j<dataParticipants.Count; j++)
+                    {
+                        if(dataMatches[i].match.player1_id.Equals(dataParticipants[j].Participant.id))
+                        {
+                            player1Name = dataParticipants[j].Participant.name;
+                        }
+                        else if (dataMatches[i].match.player2_id.Equals(dataParticipants[j].Participant.id))
+                        {
+                            player2Name = dataParticipants[j].Participant.name;
+                        }
+                    }
+                    openMatches.Add(dataMatches[i].match);
+                    
+                    if (dataMatches[i].match.round == finalRound)
+                    {
+                        round = "Grand Finals";
+                        comboBox.Items.Add(round + ": " + player1Name + " vs. " + player2Name);
+                    }
+                    else if (dataMatches[i].match.round == finalRound*-1)
+                    {
+                        round = "Losers Finals";
+                        comboBox.Items.Add(round + ": " + player1Name + " vs. " + player2Name);
+                    }
+                    else if(dataMatches[i].match.round < 0)
+                    {
+                        round = "Losers";
+                        comboBox.Items.Add(round + " Round " + Math.Abs(dataMatches[i].match.round) + ": " + player1Name + " vs. " + player2Name);
+                    }
+                    else
+                    {
+                        round = "Winners";
+                        comboBox.Items.Add(round + " Round " + Math.Abs(dataMatches[i].match.round) + ": " + player1Name + " vs. " + player2Name);
+                    }
+                    
+                }
+            }
+        }
     }
 
     public class Participants
@@ -38,7 +103,7 @@ namespace ChallongeOBSScoreKeeper
 
     public class Participant
     {
-        public string id { get; set; }
+        public int? id { get; set; }
         public string tournament_id { get; set; }
         public string name { get; set; }
         public string seed { get; set; }
@@ -67,6 +132,43 @@ namespace ChallongeOBSScoreKeeper
         public bool can_check_in { get; set; }
         public bool checked_in { get; set; }
         public bool reactivatable { get; set; }
+    }
+    public class Matches
+    {
+        public SingleMatch match { get; set; }
+    }
+
+    public class SingleMatch
+    {
+        public int id { get; set; }
+        public int tournament_id { get; set; }
+        public string state { get; set; }
+        public int? player1_id { get; set; }
+        public int? player2_id { get; set; }
+        public int? player1_prereq_match_id { get; set; }
+        public int? player2_prereq_match_id { get; set; }
+        public bool player1_is_prereq_match_loser { get; set; }
+        public bool player2_is_prereq_match_loser { get; set; }
+        public object winner_id { get; set; }
+        public object loser_id { get; set; }
+        public string started_at { get; set; }
+        public string created_at { get; set; }
+        public string updated_at { get; set; }
+        public string identifier { get; set; }
+        public bool has_attachment { get; set; }
+        public int round { get; set; }
+        public object player1_votes { get; set; }
+        public object player2_votes { get; set; }
+        public object group_id { get; set; }
+        public object attachment_count { get; set; }
+        public object scheduled_time { get; set; }
+        public object location { get; set; }
+        public object underway_at { get; set; }
+        public object optional { get; set; }
+        public object rushb_id { get; set; }
+        public object completed_at { get; set; }
+        public string prerequisite_match_ids_csv { get; set; }
+        public string scores_csv { get; set; }
     }
 }
 
